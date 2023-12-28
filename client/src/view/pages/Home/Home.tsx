a1e84a69b6312e17fe050354a275c207151a4024
sehan22 <sehanranaweera2002@gmail.com>
2023-12-29 01:33:08 +0530

    site banner updated

--- client/src/view/pages/Home/Home.tsx
+++ client/src/view/pages/Home/Home.tsx
@@ -6,7 +6,10 @@ export class Home extends Component {
         return (
             <div className="flex flex-col items-center justify-center">
                 <img className="object-cover w-screen max-h-screen" src={bannerBackgroundImg} alt=""/>
-                dshfoudf
+                <h1 className="font-Milonga text-topic">Welcome To FiesTo</h1>
+                <hr/>
+                <h6 className="font-poppins text-subtopic">Join With Us</h6>
+                <hr/>
             </div>
         );
     }
