f7cdbf64f83f5e402af32eba024b0a9c9195d45f
sehan22 <sehanranaweera2002@gmail.com>
2023-12-30 14:38:39 +0530

    site Home Page seventh div updated

--- client/src/view/pages/Home/Home.tsx
+++ client/src/view/pages/Home/Home.tsx
@@ -13,6 +13,9 @@ import SuiteRoomImg from '../../../images/HomePage/FifthDiv/suiteroom.png'
 import FamilyRoomImg from '../../../images/HomePage/FifthDiv/familyroom.png'
 import DeluxeRoomImg from '../../../images/HomePage/FifthDiv/deluxroom.png'
 import SixthDivBGImg from '../../../images/HomePage/SixthDiv/sixthdivbgimg.png'
+import FirstManImg from '../../../images/HomePage/SeventhDiv/1stman.png'
+import SecondManImg from '../../../images/HomePage/SeventhDiv/2ndman.png'
+import ThirdManImg from '../../../images/HomePage/SeventhDiv/3rdman.png'
 
 export class Home extends Component {
     render() {
@@ -378,17 +381,33 @@ export class Home extends Component {
                 </div>
 
                 {/*seventh div*/}
-                <div className="flex justify-center items-center mb-[120px] bg-octonary">
-                    <div className="flex flex-col flex-wrap justify-center items-center gap-10 p-10 bg-nonary">
-                        <div className="flex flex-col justify-center items-center bg-black p-5">
+                <div className="flex justify-center items-center mb-[120px]">
+                    <div className="flex flex-col flex-wrap justify-center items-center gap-10">
+                        <div className="flex flex-col justify-center items-center p-5">
                             <h2 className="font-poppins text-secondary text-topictwo">TESTIMONIAL</h2>
                             <h1 className="font-Milonga text-quinary text-topic">Happy Guest Comments</h1>
                         </div>
 
-                        <div className="flex flex-wrap items-center justify-center bg-black p-5 gap-5">
-                            <div className="w-[320px] h-[220px] bg-white shadow-xl"></div>
-                            <div className="w-[320px] h-[220px] bg-white shadow-xl"></div>
-                            <div className="w-[320px] h-[220px] bg-white shadow-xl"></div>
+                        <div className="flex flex-wrap items-center justify-center p-5 gap-5">
+                            <div
+                                className="flex flex-col justify-center items-start w-[320px] h-[220px] p-5 rounded-md bg-white">
+                                <div className="flex flex-col gap-3 font-poppins">
+                                    <div className="flex items-center gap-3">
+                                        <img className="w-[80px]" src={FirstManImg} alt=""/>
+
+                                        <div>
+                                            <h1 className="text-topictwo text-quinary tracking-[2px]">Roger Scott</h1>
+                                            <h6 className="text-smaller text-secondary">Marketing Manager</h6>
+                                        </div>
+                                    </div>
+
+                                    <p className="text-smaller text-senary leading-5">
+                                        Far far away, behind the word mountains, far
+                                        from the countries Vokalia and Consonantia,
+                                        there live the blind texts.
+                                    </p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
