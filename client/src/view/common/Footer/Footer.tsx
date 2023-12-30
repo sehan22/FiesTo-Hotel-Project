416aec71cbb4a82c1f1a41f2fba49b1041b33a84
sehan22 <sehanranaweera2002@gmail.com>
2023-12-30 15:03:01 +0530

    site Home Page Footer.tsx updated

--- client/src/view/common/Footer/Footer.tsx
+++ client/src/view/common/Footer/Footer.tsx
@@ -3,8 +3,14 @@ import React, {Component} from 'react';
 export class Footer extends Component {
     render() {
         return (
-            <div>
+            <div className="flex bg-gradient-to-t from-black to-primary">
+                <div className="flex flex-col items-center justify-center g-10 text-white">
+                    <div className="flex justify-center items-center text-center">
+                        <div></div>
 
+                        <h3 className="text-center">DEVELOPED BY SEHAN RANAWEERA 2023</h3>
+                    </div>
+                </div>
             </div>
         );
     }
