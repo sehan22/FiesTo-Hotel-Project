75460d12b13f860ce0abde1821bebd9762820048
sehan22 <sehanranaweera2002@gmail.com>
2024-01-01 00:01:07 +0530

    site SignUp page UI updated.

--- client/src/view/pages/SignUp/SignUp.tsx
+++ client/src/view/pages/SignUp/SignUp.tsx
@@ -18,7 +18,8 @@ export class SignUp extends Component {
                             <h6 className="text-primary">Already have an account? </h6>
 
                             <div className="flex items-center justify-center gap-1">
-                                <h6 className="text-secondary cursor-pointer" style={{fontWeight: '600'}}><Link to="/login">Sign In Now</Link></h6>
+                                <h6 className="text-secondary cursor-pointer" style={{fontWeight: '600'}}><Link
+                                    to="/login">Sign In Now</Link></h6>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="9" height="9" viewBox="0 0 6 6"
                                      fill="none">
                                     <path
@@ -29,7 +30,21 @@ export class SignUp extends Component {
                         </div>
                     </div>
 
-                    <form className="flex flex-col justify-center items-center mt-2 gap-6">
+                    <form
+                        className="flex flex-col justify-center h-80 items-center mt-2 gap-6 pe-5 overflow-y-scroll custom-scrollbar">
+                        <div className="w-80 border-gray-400">
+                            <div className="relative w-full min-w-[200px] h-10">
+                                <input
+                                    className="peer w-full h-full bg-transparent text-blue-gray-700 font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-smaller px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-gray-900"
+                                    placeholder=" "
+                                    type="text"
+                                    required/><label
+                                className="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-blue-gray-500 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-normal text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-gray-500 peer-focus:text-gray-900 before:border-blue-gray-200 peer-focus:before:!border-gray-900 after:border-blue-gray-200 peer-focus:after:!border-gray-900">Full
+                                Name
+                            </label>
+                            </div>
+                        </div>
+
                         <div className="w-80 border-gray-400">
                             <div className="relative w-full min-w-[200px] h-10">
                                 <input
@@ -53,14 +68,14 @@ export class SignUp extends Component {
                             </label>
                             </div>
                         </div>
-
-                        <div className="flex w-full">
-                            <button
-                                className="bg-secondary text-smaller text-white hover:bg-teal-600 py-2 px-4 w-full rounded">
-                                Sign In
-                            </button>
-                        </div>
                     </form>
+
+                    <div className="flex w-full">
+                        <button
+                            className="bg-secondary text-smaller text-white hover:bg-teal-600 py-2 px-4 w-full rounded">
+                            Create Account
+                        </button>
+                    </div>
 
                     <div className="flex items-center justify-center gap-2 m-auto">
                         <div className="h-0.5 w-10 bg-gray-200"></div>
