092f0fe9217205834163b9ab6f3aca3326edbab4
sehan22 <sehanranaweera2002@gmail.com>
2024-01-01 23:55:01 +0530

    site Room Page UI updated.

--- client/src/view/pages/Room/Room.tsx
+++ client/src/view/pages/Room/Room.tsx
@@ -13,12 +13,17 @@ export class Room extends Component {
                         backgroundSize: 'cover',
                         backgroundPosition: 'center'
                     }}>
-                    <h1 className="font-Milonga text-bannertopic text-white">Rooms</h1>
+                    <h1 className="font-Milonga text-bannertopic text-white">Rooms Section</h1>
                     <div className="flex items-center justify-center gap-4 p-2 ">
                         <div className="h-0.5 opacity-50 bg-septenary w-32"></div>
                         <h6 className="font-poppins text-subtopic text-nonary">Join With Us</h6>
                         <div className="h-0.5 opacity-50 bg-septenary w-32"></div>
                     </div>
+                </div>
+
+                {/*first div*/}
+                <div className="flex justify-center items-center mt-[120px] mb-[120px] bg-white font-poppins">
+                    <h5 className="text-quinary text-topictwo">Room Section</h5>
                 </div>
             </>
         );
