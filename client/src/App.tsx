0671593fff669742198dc7ea5ee231a6451936e7
sehan22 <sehanranaweera2002@gmail.com>
2024-01-04 17:26:19 +0530

    modules updated

---
+++ client/src/App.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {DefaultLayout} from "./view/common/DefaultLayout/DefaultLayout";
+import {Login} from "./view/pages/Login/Login";
+import {SignUp} from "./view/pages/SignUp/SignUp";
+
+function App() {
+    return (
+        /*<Navbar fluid rounded>
+            <Navbar.Brand href="https://flowbite-react.com">
+                <img src="/favicon.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
+                <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+            <Navbar.Collapse>
+                <Navbar.Link href="#" active>
+                    Home
+                </Navbar.Link>
+                <Navbar.Link href="#">
+                    About
+                </Navbar.Link>
+                <Navbar.Link href="#">Services</Navbar.Link>
+                <Navbar.Link href="#">Pricing</Navbar.Link>
+                <Navbar.Link href="#">Contact</Navbar.Link>
+            </Navbar.Collapse>
+        </Navbar>*/
+
+        <BrowserRouter>
+            <Routes>
+                <Route path="/*"
+                       Component={DefaultLayout}>
+                </Route>
+
+                <Route path="/login"
+                       Component={Login}>
+                </Route>
+
+                <Route path="/signup"
+                       Component={SignUp}>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
