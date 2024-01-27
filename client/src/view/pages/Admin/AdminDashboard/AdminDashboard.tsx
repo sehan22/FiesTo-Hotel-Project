1f60b5f9c310f105ac3579f3f322fbfb898163fe
sehan22 <sehanranaweera2002@gmail.com>
2024-01-27 11:36:19 +0530

    site Admin page updated.navbar, menubar and dashboard UI updated.

--- client/src/view/pages/Admin/AdminDashboard/AdminDashboard.tsx
+++ client/src/view/pages/Admin/AdminDashboard/AdminDashboard.tsx
@@ -4,18 +4,21 @@ import UsersIcon from '../../../../images/Admin/Dashboard/usersicon.png'
 import OrdersIcon from '../../../../images/Admin/Dashboard/ordersicon.png'
 import CompleteOrderIcon from '../../../../images/Admin/Dashboard/completeordericon.png'
 
+import {Chart} from 'chart.js'
+
+
 class AdminDashboard extends Component {
     render() {
         return (
             <div className="flex justify-center items-center p-5 w-full mx-auto font-poppins">
-                <div className="flex justify-between items-center px-10 py-2 gap-5 w-full rounded-xl">
+                <div className="flex flex-col justify-center items-center px-10 py-2 gap-5 w-full rounded-xl">
 
                     {/*main grid*/}
                     <div className="grid grid-cols-4 gap-8 w-full">
 
                         {/*grid item*/}
                         <div className="flex justify-center items-center bg-white p-10 rounded-xl">
-                            <div className="flex justify-center items-center gap-16">
+                            <div className="flex justify-between items-center gap-16">
                                 <div className="flex flex-col items-start justify-center">
                                     <h6 className="text-smaller text-senary">Budgets</h6>
                                     <h1 className="text-[30px]">$48.5</h1>
@@ -29,7 +32,7 @@ class AdminDashboard extends Component {
                         </div>
 
                         <div className="flex justify-center items-center bg-white p-10 rounded-xl">
-                            <div className="flex justify-center items-center gap-16">
+                            <div className="flex justify-between items-center gap-16">
                                 <div className="flex flex-col items-start justify-center">
                                     <h6 className="text-smaller text-senary">Users</h6>
                                     <h1 className="text-[30px]">8.5K</h1>
@@ -43,7 +46,7 @@ class AdminDashboard extends Component {
                         </div>
 
                         <div className="flex justify-center items-center bg-white p-10 rounded-xl">
-                            <div className="flex justify-center items-center gap-16">
+                            <div className="flex justify-between items-center gap-16">
                                 <div className="flex flex-col items-start justify-center">
                                     <h6 className="text-smaller text-senary">Orders</h6>
                                     <h1 className="text-[30px]">$48.5</h1>
@@ -57,7 +60,7 @@ class AdminDashboard extends Component {
                         </div>
 
                         <div className="flex justify-center items-center bg-white p-10 rounded-xl">
-                            <div className="flex justify-center items-center gap-16">
+                            <div className="flex justify-between items-center gap-16">
                                 <div className="flex flex-col items-start justify-center">
                                     <h6 className="text-smaller text-senary">Completed Orders</h6>
                                     <h1 className="text-[30px]">$48.5</h1>
@@ -70,6 +73,12 @@ class AdminDashboard extends Component {
                             </div>
                         </div>
 
+                    </div>
+
+
+                    <div className="flex justify-between items-center gap-8 w-full">
+                        <div className="w-[60%] bg-white rounded-xl p-5"></div>
+                        <div className="w-[40%] bg-white rounded-xl p-5"></div>
                     </div>
 
                 </div>
