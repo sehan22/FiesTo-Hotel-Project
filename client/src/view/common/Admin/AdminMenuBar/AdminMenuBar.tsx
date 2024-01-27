329173d80a1de8a25f8cebe46a7e689c29383b53
sehan22 <sehanranaweera2002@gmail.com>
2024-01-27 10:39:30 +0530

    site Admin page updated.navbar, menubar and dashboard UI updated.

--- client/src/view/common/Admin/AdminMenuBar/AdminMenuBar.tsx
+++ client/src/view/common/Admin/AdminMenuBar/AdminMenuBar.tsx
@@ -7,20 +7,20 @@ class AdminMenuBar extends Component {
     render() {
         return (
             <div className="flex justify-center items-center p-5 w-full font-poppins">
-                <div className="flex justify-between items-center w-full bg-white px-10 py-2 shadow-md rounded-3xl">
+                <div className="flex justify-between items-center w-full bg-white px-5 py-2 shadow-sm rounded-3xl">
                     <h1 className="text-quinary text-loginpagetopic">Admin Dashboard</h1>
 
                     <div className="flex justify-center items-center gap-5">
 
                         <div className="flex justify-center items-center gap-2">
                             <img className="w-10 h-10" src={UserIcon} alt=""/>
-                            <h1 className="text-normal">adminsehan</h1>
+                            <h1 className="text-normal text-quinary">adminsehan</h1>
                         </div>
 
                         <div className="flex justify-center items-center gap-2">
 
                             <img className="w-6 h-6 object-cover" src={BellIcon} alt=""/>
-                            <img className="w-6 h-6 object-cover" src={SettingIcon} alt=""/>
+                            <img className="w-6 h-6 object-cover hover:animate-spin transition-all opacity-70 hover:opacity-80" src={SettingIcon} alt=""/>
                         </div>
 
                     </div>
--- client/src/view/pages/Admin/AdminDashboard/AdminDashboard.tsx
+++ client/src/view/pages/Admin/AdminDashboard/AdminDashboard.tsx
@@ -1,12 +1,76 @@
 import React, {Component} from 'react';
+import DollarIcon from '../../../../images/Admin/Dashboard/dollaricon.png'
+import UsersIcon from '../../../../images/Admin/Dashboard/usersicon.png'
+import OrdersIcon from '../../../../images/Admin/Dashboard/ordersicon.png'
+import CompleteOrderIcon from '../../../../images/Admin/Dashboard/completeordericon.png'
 
 class AdminDashboard extends Component {
     render() {
         return (
-            <div className="flex justify-center items-center p-5 w-full mx-auto ">
-                <div className="flex justify-between items-center p-5 gap-5 w-full bg-white shadow-sm rounded-xl">
+            <div className="flex justify-center items-center p-5 w-full mx-auto font-poppins">
+                <div className="flex justify-between items-center px-10 py-2 gap-5 w-full rounded-xl">
 
-                    <div></div>
+                    {/*main grid*/}
+                    <div className="grid grid-cols-4 gap-8 w-full">
+
+                        {/*grid item*/}
+                        <div className="flex justify-center items-center bg-white p-10 rounded-xl">
+                            <div className="flex justify-center items-center gap-16">
+                                <div className="flex flex-col items-start justify-center">
+                                    <h6 className="text-smaller text-senary">Budgets</h6>
+                                    <h1 className="text-[30px]">$48.5</h1>
+                                    <p className="text-smaller">Budget Summary</p>
+                                </div>
+
+                                <div className="flex justify-center w-16 h-16 items-center p-5 bg-red-500 rounded-full">
+                                    <img className="bg-white p-1 rounded-full" src={DollarIcon} alt=""/>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="flex justify-center items-center bg-white p-10 rounded-xl">
+                            <div className="flex justify-center items-center gap-16">
+                                <div className="flex flex-col items-start justify-center">
+                                    <h6 className="text-smaller text-senary">Users</h6>
+                                    <h1 className="text-[30px]">8.5K</h1>
+                                    <p className="text-smaller">Users Summary</p>
+                                </div>
+
+                                <div className="flex justify-center w-16 h-16 items-center p-5 bg-teal-500 rounded-full">
+                                    <img className="bg-white p-1 rounded-full" src={UsersIcon} alt=""/>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="flex justify-center items-center bg-white p-10 rounded-xl">
+                            <div className="flex justify-center items-center gap-16">
+                                <div className="flex flex-col items-start justify-center">
+                                    <h6 className="text-smaller text-senary">Orders</h6>
+                                    <h1 className="text-[30px]">$48.5</h1>
+                                    <p className="text-smaller">Orders Summary</p>
+                                </div>
+
+                                <div className="flex justify-center w-16 h-16 items-center p-5 bg-orange-500 rounded-full">
+                                    <img className="bg-white p-1 rounded-full" src={OrdersIcon} alt=""/>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="flex justify-center items-center bg-white p-10 rounded-xl">
+                            <div className="flex justify-center items-center gap-16">
+                                <div className="flex flex-col items-start justify-center">
+                                    <h6 className="text-smaller text-senary">Completed Orders</h6>
+                                    <h1 className="text-[30px]">$48.5</h1>
+                                    <p className="text-smaller">Orders Summary</p>
+                                </div>
+
+                                <div className="flex justify-center w-16 h-16 items-center p-5 bg-indigo-500 rounded-full">
+                                    <img className="bg-white p-1 rounded-full" src={CompleteOrderIcon} alt=""/>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
 
                 </div>
             </div>
